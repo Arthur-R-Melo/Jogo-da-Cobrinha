add5980c43d2862ac2bb2406fc69d7dc54f4efbc
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2025-10-18 23:38:46 +0000

    Implement snake.c

--- snake.c
+++ snake.c
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "snake.h"
+
+void insertInHead(Body* cobra, Coord coord){
+    NodePointer temp = malloc(sizeof(Node));
+    temp->prox = NULL;
+    temp->coord = coord;
+    cobra->head->prox = temp;
+    cobra->head = temp;
+}
+
+void removeFromTail(Body* cobra){
+    if (cobra->tail->prox == NULL)
+    {
+        printf("Error: nothing to remove");
+        return;
+    }
+    
+    NodePointer temp = cobra->tail;
+    cobra->tail = cobra->tail->prox;
+    free(temp);
+}
--- snake.h
+++ snake.h
@@ -5,11 +5,31 @@
 
 #define SNAKE_COLOR YELLOW
 
+typedef struct Node *NodePointer;
+
+typedef struct Coord{
+    int x;
+    int y;
+}Coord;
+
+
+typedef struct Node{
+    NodePointer prox;
+    Coord coord;
+}Node;
+
+/*
+* O começo da estrutura Body se dá na cauda
+*/
 typedef struct Body{
+    NodePointer head;
+    NodePointer tail;
     Rectangle pos;
     Color color;
     int direcao;
 }Body;
 
+void insertInHead(Body* cobra, Coord coord);
+void removeFromTail(Body* cobra);
 
 #endif
