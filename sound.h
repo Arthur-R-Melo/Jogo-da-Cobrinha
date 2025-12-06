6f8181ee5320caebcedf9525a883b660b10d4e47
Osant-anna <jvsantanna11@protonmail.com>
2025-12-06 06:08:53 +0000

    Add sound functions and header guard in sound.h

--- sound.h
+++ sound.h
@@ -1,1 +1,11 @@
+#ifndef SOUND_H
+#define SOUND_H
+#include "jogo.h"
 
+void CarregaMusicaMenu(Jogo* jogo);
+void DescarregaMusicaMenu(Jogo* jogo);
+void CarregaSons(Jogo* jogo);
+void DescarregaSons(Jogo* jogo);
+void CarregaMusicaDif(Jogo* jogo);
+void DescarregaMusicaDif(Jogo* jogo);
+#endif
